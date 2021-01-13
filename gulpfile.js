f4876f794f0d9a7a3edd27bdbe238958319b82ba
Guillem Garcia <guillemgarciagomez@gmail.com>
2021-01-13 12:54:45 +0100

    feat: Added server setup
    Installed what at this point seem all the required libraries

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,8 @@ const $ = require('gulp-load-plugins')();
 function addDefSrcIgnore (srcArr) {
   return srcArr.concat([
     '!node_modules{,/**}',
-    '!private{,/**}',
-    '!dist{,/**}',
     '!.git{,/**}',
-    '!**/.DS_Store'
+    '!**/node_modules{,/**}'
   ]);
 }
 
