01f01275a2a6ed76d4e9c7ef2dbf462679291281
Guillem Garcia <guillemgarciagomez@gmail.com>
2021-01-18 16:21:47 +0100

    feat: Playing around with sw

--- client/src/sw.js
+++ client/src/sw.js
@@ -1,6 +1,13 @@
 import { getFiles, setupPrecaching, setupRouting } from 'preact-cli/sw/';
+import { registerRoute } from 'workbox-routing';
+import { StaleWhileRevalidate } from 'workbox-strategies';
+
+registerRoute(
+  ({ request }) => request.destination === 'script' ||
+    request.destination === 'style',
+  new StaleWhileRevalidate()
+);
 
 setupRouting();
-console.log('getFiles()', getFiles());
 setupPrecaching(getFiles());
 
