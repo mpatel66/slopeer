879c88676a82c3d31997a7d675b35a97c8ad6970
Guillem Garcia <guillemgarciagomez@gmail.com>
2021-01-20 10:35:57 +0100

    feat: Added config example for frontend

---
+++ client/config.example.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mapboxToken: '',
+  mapboxStyle: '',
+  serverUrl: '',
+  profilePicturesUrl: 'https://slopeer-public.s3.eu-central-1.amazonaws.com/profile_pictures/',
+  routePicturesUrl: 'https://slopeer-public.s3.eu-central-1.amazonaws.com/route_pictures/'
+};
